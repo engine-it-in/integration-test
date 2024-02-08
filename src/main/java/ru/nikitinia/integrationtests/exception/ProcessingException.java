fbf859cd1f17ab97df99a4298226df2d1a527678
Ivan <53277653+engine-it-in@users.noreply.github.com>
2024-02-08 09:13:12 +0300

    Create ProcessingException.java

---
+++ src/main/java/ru/nikitinia/integrationtests/exception/ProcessingException.java
@@ -0,0 +1,8 @@
+package ru.alfastrah.odm.integrationtests.exception;
+
+public class ProcessingException extends RuntimeException{
+
+    public ProcessingException(String message) {
+        super(message);
+    }
+}
