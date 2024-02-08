878afde3aa05daab5de883c6d0677209998d1b00
Ivan <53277653+engine-it-in@users.noreply.github.com>
2024-02-08 10:46:16 +0300

    Create EnvironmentProperty.java

---
+++ src/main/java/ru/nikitinia/integrationtests/model/configproperty/EnvironmentProperty.java
@@ -0,0 +1,15 @@
+package ru.nikitinia.integrationtests.model.configproperty;
+
+import lombok.Builder;
+
+/**
+ * Объект для работы с:
+ *  Системными переменными - EnvironmentProperty environmentProperty (env.properties)
+ *  Например, данные для подключения к vault
+ */
+@Builder
+public record EnvironmentProperty(
+        String vaultRoleId,
+        String vaultSecretId
+) {
+}
