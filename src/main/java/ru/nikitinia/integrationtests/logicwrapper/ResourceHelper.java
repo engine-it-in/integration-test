8c02c80b9d4d437f66f206d628ab42c95313b515
Ivan <53277653+engine-it-in@users.noreply.github.com>
2024-02-08 09:47:03 +0300

    Create ResourceHelper.java

---
+++ src/main/java/ru/nikitinia/integrationtests/logicwrapper/ResourceHelper.java
@@ -0,0 +1,31 @@
+package ru.nikitinia.integrationtests.logicwrapper;
+
+import com.google.common.io.Resources;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@UtilityClass
+public class ResourceHelper {
+
+    /**
+     * Загрузить содержимое файла
+     *
+     * @param resourceFile - путь к файлу
+     * @return содержимое файла
+     */
+    public static String loadJsonData(String resourceFile) {
+        String resourceContent = null;
+        try {
+            resourceContent = Resources.toString(
+                    Resources.getResource(resourceFile),
+                    StandardCharsets.UTF_8
+            );
+        } catch (Exception e) {
+            log.error("Unable to load resource {}: {}", resourceFile, e.getMessage());
+        }
+        return resourceContent;
+    }
+}
