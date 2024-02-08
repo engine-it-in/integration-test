55ddc7684b0cd17bb539c681a79c5e2261192de5
Ivan <53277653+engine-it-in@users.noreply.github.com>
2024-02-08 09:45:33 +0300

    Create RequestMediator.java

---
+++ src/main/java/ru/nikitinia/integrationtests/logicwrapper/RequestMediator.java
@@ -0,0 +1,35 @@
+package ru.nikitinia.integrationtests.logicwrapper;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+import static io.restassured.RestAssured.requestSpecification;
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+@Slf4j
+@UtilityClass
+public class RequestMediator {
+
+    /**
+     * Настройка спецификатора для выполнения запросов Rest Assured
+     * Настройка для логирования запроса/ответа, если валидация была не успешна
+     *
+     * @param uri - путь выполнения запроса
+     */
+    public static void initRequestSpecification(String uri) {
+        requestSpecification = new RequestSpecBuilder()
+                .setContentType(APPLICATION_JSON_VALUE)
+                .setBaseUri(uri)
+                .build();
+
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        if (RestAssured.filters().isEmpty()) {
+            RestAssured.filters(new AllureRestAssured());
+        }
+    }
+
+}
