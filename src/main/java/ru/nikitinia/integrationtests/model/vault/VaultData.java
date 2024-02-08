e458e30d40f21db83dedb9f2fc318ef8a691e4a5
Ivan <53277653+engine-it-in@users.noreply.github.com>
2024-02-08 11:39:03 +0300

    Create VaultData.java

---
+++ src/main/java/ru/nikitinia/integrationtests/model/vault/VaultData.java
@@ -0,0 +1,15 @@
+package ru.alfastrah.odm.integrationtests.model.vault;
+
+import lombok.Builder;
+
+/**
+ * Объект для работы с атрибутами, необходимыми для подключения к хранилищу секретов
+ */
+@Builder
+public record VaultData(
+
+        String role_id,
+        String secret_id
+
+) {
+}
